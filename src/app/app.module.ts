95f3ee0ccd2d250e79a928f722967dcb795e21b3
amayk <amay.kalekar@gmail.com>
2019-01-05 12:53:21 +0530

    re commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,14 @@ import { AppComponent } from './app.component';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
 import { MyProfileComponent } from './components/my-profile/my-profile.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CreateProfileComponent } from './components/create-profile/create-profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingPageComponent,
-    MyProfileComponent
+    MyProfileComponent,
+    CreateProfileComponent
   ],
   imports: [
     MaterializeModule ,
