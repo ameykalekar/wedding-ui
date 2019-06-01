282c2c7584e822578f9c70fbce00796b520ea5c9
ajinkyasagar <sagarthakare1@gmail.com>
2019-06-01 11:17:59 +0530

    committing payment changes

---
+++ src/app/services/payment.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpHeaders, HttpClient } from '@angular/common/http';
+import { ResponseVo } from '../vo/responsevo';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PaymentService {
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+    , withCredentials: true
+  };
+
+
+  insertPayment(pay: ResponseVo) {
+    return this.http.post('/api/profile', JSON.stringify(pay), this.httpOptions);
+  }
+}
