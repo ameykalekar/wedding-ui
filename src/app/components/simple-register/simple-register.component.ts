4e0a697500aef964c9ab018fbb165f2fe0c386bb
ajinkyasagar <sagarthakare1@gmail.com>
2019-06-01 20:35:42 +0530

    changes simple user and added confirmation message and form reset

--- src/app/components/simple-register/simple-register.component.ts
+++ src/app/components/simple-register/simple-register.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { ProfileServiceService} from '../../services/profile-service.service';
+import { ProfileServiceService } from '../../services/profile-service.service';
 import { ProfileVo } from '../../vo/profile-vo';
+import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-simple-register',
   templateUrl: './simple-register.component.html',
@@ -8,29 +9,31 @@ import { ProfileVo } from '../../vo/profile-vo';
 })
 export class SimpleRegisterComponent implements OnInit {
 
-  showProcessing:boolean=false;
-  dateOfBirth:any;
+  showProcessing: boolean = false;
+  dateOfBirth: any;
+  usercreated = false;
 
   constructor(private profileService: ProfileServiceService) { }
 
   ngOnInit() {
   }
 
-  onDateChanged(event){
+  onDateChanged(event) {
     this.dateOfBirth = event.formatted;
   }
 
-  onClickSubmit(profile:ProfileVo){
+  onClickSubmit(profile: ProfileVo, form: NgForm) {
 
     console.log(profile);
-    
+
     profile.dateOfBirth = this.dateOfBirth;
     this.showProcessing = true;
-    this.profileService.insertSimpleProfile(profile).subscribe(res=>
-    {
-      this.showProcessing= false;
+    this.profileService.insertSimpleProfile(profile).subscribe(res => {
+      this.showProcessing = false;
       console.log(res);
+      this.usercreated = true;
 
+      form.resetForm();
     });
   }
 
